4d457a849853d17530895fbc8a3eb99c5117b494
kostyachum_mcr <kostiantyn@mychinaroots.com>
2022-02-11 14:43:23 +0200

    add app config

---
+++ app_bookmarks/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class AppBookmarksConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
+    name = 'app_bookmarks'
+    label = 'app_bookmarks'
+    verbose_name = 'App Bookmarks'
